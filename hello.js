00c892865679d30ba29f4d7ffe487d6c7751effe
namannaik <69718673+namannaik@users.noreply.github.com>
2020-08-28 14:27:08 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+﻿
+(function (window) {
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+
+
+  helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+};
+window.helloSpeaker = helloSpeaker;
+})(window);
